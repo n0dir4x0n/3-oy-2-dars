546458a98c943ec7178eee2cefb3c426e4f6c21c
NODIRA <nodira@NODIRAs-MacBook-Air.local>
2022-11-18 14:50:46 +0500

    js biography

---
+++ app.js
@@ -0,0 +1,17 @@
+let ism= prompt ('FirstName:');
+let familya= prompt ("LastName:");
+let yosh= prompt ("Age:");
+let yashashmanzil= prompt ("Adress:");
+let malumoti= prompt ("Degree:");
+let telraqam= prompt ("TelNumber:");
+let email= prompt ("Email");
+let pasportseriya= prompt("Passportseriya");
+
+alert(`FirstName : ${ism} \n
+LastName : ${familya} \n
+Age : ${yosh} \n 
+Adress : ${yashashmanzil} \n
+Degree : ${malumoti}
+TelNumber : ${telraqam} \n
+Email : ${email} \n
+Passportseriya : ${pasportseriya} `);
